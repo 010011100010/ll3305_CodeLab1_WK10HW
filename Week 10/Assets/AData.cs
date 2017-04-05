5ee9bb0881d9b357b645cba84a163d874f2f691f
010011100010 <ll3305@nyu.edu>
2017-04-05 14:50:57 -0500

    Some not working codes

---
+++ Week 10/Assets/AData.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+
+public class AData {
+
+	public Vector3 position;
+	public Vector3 scale;
+
+	private const string POS_X = "xpos";
+	private const string POS_Y = "ypos";
+	private const string POS_Z = "zpos";
+
+	public AData(string fileName){
+		JSONNode jason = UtilScript.ReadJSONFromFile (Application.dataPath, fileName);
+		position = new Vector3(jason[POS_X].AsFloat, jason[POS_Y].AsFloat, jason[POS_Z].AsFloat);
+
+	}
+		
+	public AData(Vector3 position, Vector3 scale){
+		this.position = position;
+		this.scale = scale;
+	}
+
+	public JSONClass toJSON(){
+		JSONClass json = new JSONClass ();
+		json [POS_X].AsFloat = position.x;
+		json [POS_Y].AsFloat = position.y;
+		json [POS_Z].AsFloat = position.z;
+		return json;
+	}
+
+	public void Save() {
+	
+		JSONClass json = toJSON ();
+		UtilScript.WriteJSONtoFile (Application.dataPath, fileName, json);
+	
+	}
+
+}
+
---
+++ Week 10/Assets/Saver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+
+public class Saver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject[] As = GameObject.FindGameObjectsWithTag ("A");
+		Vector3 pos = As [0].transform.position;
+		Vector3 scale = As [0].transform.localScale;
+
+		/*JSONClass json = new JSONClass ();
+		json ["xPos"].AsFloat = pos.x;
+		json ["yPos"].AsFloat = pos.y;*/
+
+		AData ad = new AData (pos, scale);
+		ad.Save ("a.txt");
+
+		AData loadAd = new AData ("a.txt");
+		print (loadAd.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
