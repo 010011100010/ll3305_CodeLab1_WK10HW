77d0ead4d8240b86b75ded28e8e5fea4d3fc0237
010011100010 <ll3305@nyu.edu>
2017-04-05 13:33:18 -0500

    Started the project

---
+++ Week 10/Assets/Loader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader  {
+
+	public float temp;
+	public string name;
+	public GameObject gameObject;
+	public int mattParker;
+	public bool isCool;
+	public Wk10Manager Manager;
+
+	public Loader(){
+		Debug.Log ("Make a loader");
+		temp = 10;
+		name = "No Name";
+		mattParker = 11;
+		isCool = false;
+	}//This is an empty function without type called default constructor
+	//An empty default constructor will be automatically created when a new object of its class is called
+	//It is recommanded to manully build a default constructor
+
+	public Loader (float temp, string name){//"This" refers to the object that is currently inside of
+		this.temp = temp;
+		this.name = name;
+	}
+}
---
+++ Week 10/Assets/Wk10Manager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wk10Manager : MonoBehaviour {
+	Loader myLoader; //Declare varible "myLoader" as Loader type
+	// Use this for initialization
+	void Start () {
+		myLoader = new Loader (0.123f, "Hey");//initialize "myLoader" as a new object of class Loader
+		//myLoader.temp = 0.123F;
+		myLoader.Manager = this;
+		print(myLoader.name);
+		print (this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
