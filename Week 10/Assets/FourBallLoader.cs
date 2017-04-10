2d113b5178af7d3bff0b78da1f466d8ce95388a4
010011100010 <ll3305@nyu.edu>
2017-04-10 16:52:21 -0500

    Working on loading and parsing JSON from txt file

--- Week 10/Assets/FourBallLoader.cs
+++ Week 10/Assets/FourBallLoader.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SimpleJSON;
 
 public class FourBallLoader : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		JSONArray position = JSON.Parse (UtilScript.ReadStringFromFile (Application.dataPath, "c.txt"));
+		print (position);
+
+		//GameObject ball1 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+
 		
 	}
 	
